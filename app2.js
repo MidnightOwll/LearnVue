15ad1892cf15053e2c2b62734003e91ddf21fc66
MidnightOwll <dv92040@gmail.com>
2021-08-08 12:33:00 +0300

    3

--- app2.js
+++ app2.js
@@ -1,12 +1,34 @@
 Vue.createApp({
 	data: () => ({
 		title: 'Title',
-		html: '<h1>Vue 3</h1>'		
+		html: '<h1>Vue 3</h1>',
+		person: {
+			firstName: 'Vitaly',
+			lastName: 'Demidenko',
+			age: 22
+		},
+		items: [1, 2, 3, 4, 5, 6, 7]		
 	}),
 	methods: {
+		// stop(event) {
+		// 	event.stopPropagation()
+		// },
+		addItem(event) {
+			this.items.unshift(this.$refs.myInput.value)
+			this.$refs.myInput.value = ''
+			console.log(event.key)
+		}, 
+		remove(i) {
+			this.items.splice(i,1)
+		},
+		log(item) {
+			console.log('Log item: ', item)
+		}
 	
 	},
 	computed: {
-		
+		evenItems() {
+			return this.items.filter(i => i % 2 === 0)
+		}
 	}
 }).mount('#app')
